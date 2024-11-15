b1365c6ad3be485f1a7f1469290dab65f4a9f857
Abhishek Chaurasiya <abhichek2580@gmail.com>
2024-11-15 10:33:45 +0530

    url changed

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: `https://emp-management-frontend-tau.vercel.app`,
+  credentials: true
+}));
 
 // /dev/api/v1/auth
 app.use(admin, authRouter);
