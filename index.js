dc2ecfc2af3a0cef28cc75b4aef4a7f6f67c1a71
abhishekchaurasiya <abhichek2580@gmail.com>
2025-07-07 07:46:04 +0530

    delete varcel.json file

--- index.js
+++ index.js
@@ -19,9 +19,20 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigins = [
+  process.env.CLIENT_WEB_URL,
+  process.env.CLIENT_LOCALHOST_URL,
+];
+
 const corsOptions = {
-  origin: "https://emp-management-frontend-tau.vercel.app", // Your frontend origin
-  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true, // Allow cookies if needed
 };
 
@@ -42,3 +53,5 @@ app.listen(PORT, () => {
 app.get("/", async (req, res) => {
   res.json("Server is running");
 });
+
+// password: Abhi123@#
